ee1a2293f3217eacdd3db1c1db3444f9c067061d
kristijankralj <kristijan.kralj@live.com>
2021-06-14 21:59:48 +0200

    Remove main page

---
+++ DreamProperties/DreamProperties/Application/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DreamProperties.Modules.Login;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DreamProperties
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+    }
+}
