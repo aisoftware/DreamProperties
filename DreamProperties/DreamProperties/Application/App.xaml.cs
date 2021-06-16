6a261880ba096ab364aa9ebf9ac65e6d8b976b62
kristijankralj <kristijan.kralj@live.com>
2021-06-16 09:06:57 +0200

    Add custom fonts

--- DreamProperties/DreamProperties/Application/App.xaml.cs
+++ DreamProperties/DreamProperties/Application/App.xaml.cs
@@ -3,6 +3,8 @@ using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "AppRegular")]
+[assembly: ExportFont("Poppins-Medium.ttf", Alias = "AppMedium")]
 namespace DreamProperties
 {
     public partial class App : Application
