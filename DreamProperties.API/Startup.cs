80e3d76b6e114a623965cd2e1429cab813b59506
kristijankralj <kristijan.kralj@live.com>
2021-06-20 19:37:24 +0200

    Start with FB login

--- DreamProperties.API/Startup.cs
+++ DreamProperties.API/Startup.cs
@@ -17,12 +17,15 @@ namespace DreamProperties.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
             Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
         }
 
-        public IConfiguration Configuration { get; }
+        IConfiguration Configuration { get; }
+
+        IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -37,7 +40,39 @@ namespace DreamProperties.API
             services.AddAuthentication(o =>
             {
                 o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddFacebook(facebook =>
+            {
+                //facebook.AppId = Configuration["Authentication:Facebook:AppId"];
+                //facebook.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                facebook.AppId = "517650452603306";
+                facebook.AppSecret = "0fda4635bf1428b3bf1c22da6e907d73";
+                facebook.SaveTokens = true;
+            })
+            .AddGoogle(google =>
+            {
+                google.ClientId = Configuration["Authentication:Google:ClientId"];
+                google.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+                google.SaveTokens = true;
+            })
+            .AddApple(apple =>
+            {
+                apple.ClientId = Configuration["AppleClientId"];
+                apple.KeyId = Configuration["AppleKeyId"];
+                apple.TeamId = Configuration["AppleTeamId"];
+                apple.UsePrivateKey(keyId
+                    => WebHostEnvironment.ContentRootFileProvider.GetFileInfo($"AuthKey_{keyId}.p8"));
+                apple.SaveTokens = true;
             });
+            /*
+            * For Apple signin
+            * If you are running the app on Azure you must add the Configuration setting
+            * WEBSITE_LOAD_USER_PROFILE = 1
+            * Without this setting you will get a 
+            * File Not Found exception when AppleAuthenticationHandler tries 
+            * to generate a certificate using your Auth_{keyId].P8 file.
+            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +89,7 @@ namespace DreamProperties.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- DreamProperties/DreamProperties/Modules/Login/LoginViewModel.cs
+++ DreamProperties/DreamProperties/Modules/Login/LoginViewModel.cs
@@ -1,10 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Essentials;
 
 namespace DreamProperties.Modules.Login
 {
     public class LoginViewModel
     {
+        private const string AUTHENTICATION_URL = "https://https://dreamproperties.azurewebsites.net/auth/";
+
+        public AsyncCommand FacebookAuthCommand { get => new AsyncCommand(FacebookAuthenticate); }
+
+        public AsyncCommand GoogleAuthCommand { get => new AsyncCommand(GoogleAuthenticate); }
+        public AsyncCommand AppleAuthCommand { get => new AsyncCommand(AppleAuthenticate); }
+
+        private Task FacebookAuthenticate()
+        {
+            return PerformAuthentication("Facebook");
+        }
+
+        private Task GoogleAuthenticate()
+        {
+            return PerformAuthentication("Google");
+        }
+
+        private Task AppleAuthenticate()
+        {
+            return PerformAuthentication("Apple");
+        }
+
+        private async Task PerformAuthentication(string scheme)
+        {
+            try
+            {
+                var authUrl = new Uri(AUTHENTICATION_URL + scheme);
+                var callbackUrl = new Uri("xamdream://");
+
+                var result = await WebAuthenticator.AuthenticateAsync(authUrl, callbackUrl);
+
+                string authToken = result.AccessToken;
+                string refreshToken = result.RefreshToken;
+                var jwtTokenExpiresIn = result.Properties["jwt_token_expires"];
+
+                var userInfo = new Dictionary<string, string>
+                {
+                    { "token", authToken },
+                    { "name", $"{result.Properties["firstName"]} {result.Properties["secondName"]}"},
+                };
+            }
+            catch (TaskCanceledException)
+            {
+                //User canceled auth flow;
+            }
+        }
     }
 }
