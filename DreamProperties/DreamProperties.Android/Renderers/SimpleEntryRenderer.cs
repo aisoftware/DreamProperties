a310e782cb4b634973c17080cac35a2a64154b15
kristijankralj <kristijan.kralj@live.com>
2021-06-26 09:19:55 +0200

    Add search icon

---
+++ DreamProperties/DreamProperties.Android/Renderers/SimpleEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using DreamProperties.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(Entry), typeof(SimpleEntryRenderer))]
+namespace DreamProperties.Droid.Renderers
+{
+    public class SimpleEntryRenderer: EntryRenderer
+    {
+        public SimpleEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
---
+++ DreamProperties/DreamProperties.iOS/Renderers/SimpleEntryRenderer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace DreamProperties.iOS.Renderers
+{
+    public class SimpleEntryRenderer: EntryRenderer
+    {
+    }
+}
