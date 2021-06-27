08288b1e7dece2548babb3723562b36019459d58
kristijankralj <kristijan.kralj@live.com>
2021-06-27 20:20:59 +0200

    Dark mode for login view

--- DreamProperties/DreamProperties/Application/App.xaml.cs
+++ DreamProperties/DreamProperties/Application/App.xaml.cs
@@ -29,7 +29,7 @@ namespace DreamProperties
             //get container
             Container = builder.Build();
 
-            MainPage = new AppShell(); //Container.Resolve<LoginView>();
+            MainPage = new AppShell();
         }
     }
 }
