e3f12697870f21f79eba3c45c9b8d406862c69cc
Kristijan Kralj <kristijan.kralj@live.com>
2021-06-27 12:53:48 +0200

    Update entry renderer

--- DreamProperties/DreamProperties.iOS/Renderers/SimpleEntryRenderer.cs
+++ DreamProperties/DreamProperties.iOS/Renderers/SimpleEntryRenderer.cs
@@ -1,14 +1,23 @@
-﻿using Foundation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using DreamProperties.Common.CustomControls;
+using DreamProperties.iOS.Renderers;
 using UIKit;
+using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
+[assembly: ExportRenderer(typeof(SimpleEntry), typeof(SimpleEntryRenderer))]
 namespace DreamProperties.iOS.Renderers
 {
     public class SimpleEntryRenderer: EntryRenderer
     {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Background = null;
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
     }
 }
