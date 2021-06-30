5a903d310be52b136796ef7c140181dc0fa8c47b
kristijankralj <kristijan.kralj@live.com>
2021-06-30 09:03:16 +0200

    Add Serilog

--- DreamProperties.API/Program.cs
+++ DreamProperties.API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,20 @@ namespace DreamProperties.API
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+               .Enrich.FromLogContext()
+               .WriteTo.File(
+                path: "logs\\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information
+               )
+               .CreateLogger();
+
+            Log.Information("Application is starting");
+
             CreateHostBuilder(args).Build().Run();
         }
 
