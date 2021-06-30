3d75734fe9b7e6cbab38f589eb83646da06732ce
kristijankralj <kristijan.kralj@live.com>
2021-06-30 06:45:05 +0200

    Finish add property

--- DreamProperties/DreamProperties/Modules/AddProperty/AddPropertyViewModel.cs
+++ DreamProperties/DreamProperties/Modules/AddProperty/AddPropertyViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DreamProperties.Common.Base;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.ObjectModel;
@@ -11,6 +12,7 @@ namespace DreamProperties.Modules.AddProperty
     public class AddPropertyViewModel : BaseViewModel
     {
         private string _propertyType = string.Empty;
+        private List<string> _amenities = new List<string>();
 
         public AddPropertyViewModel()
         {
@@ -22,6 +24,8 @@ namespace DreamProperties.Modules.AddProperty
         public Command<string> AmenityCommand { get => new Command<string>(SelectAmenity); }
 
         public AsyncCommand GetLocationCommand { get => new AsyncCommand(GetLocation); }
+
+        public AsyncCommand CreatePropertyCommand { get => new AsyncCommand(CreateProperty); }
 
         private string _address;
         public string Address
@@ -41,18 +45,37 @@ namespace DreamProperties.Modules.AddProperty
             }
         }
 
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
         private string _selectedAmenities;
-        public string SelectedAmenities { get => _selectedAmenities; set => SetProperty(ref _selectedAmenities, value); }
+        public string SelectedAmenities
+        { 
+            get => _selectedAmenities;
+            set => SetProperty(ref _selectedAmenities, $"Selected: {value}");
+        }
 
         private string _typeSelection;
         public string TypeSelection
         {
             get => _typeSelection;
-            set
-            {
-                string newValue = $"Currently selected: {value}";
-                SetProperty(ref _typeSelection, newValue);
-            }
+            set => SetProperty(ref _typeSelection, $"Selected: {value}");
+        }
+
+        private double _numberOfBedrooms = 1;
+        public double NumberOfBedrooms
+        {
+            get => _numberOfBedrooms;
+            set => SetProperty(ref _numberOfBedrooms, value);
+        }
+
+        private Task CreateProperty()
+        {
+            return Task.CompletedTask;
         }
 
         private async Task GetLocation()
@@ -98,9 +121,19 @@ namespace DreamProperties.Modules.AddProperty
             TypeSelection = _propertyType;
         }
 
+
         private void SelectAmenity(string selected)
         {
-            throw new NotImplementedException();
+            if (_amenities.Contains(selected))
+            {
+                _amenities.Remove(selected);
+            }
+            else
+            {
+                _amenities.Add(selected);
+            }
+
+            SelectedAmenities = String.Join(", ", _amenities);
         }
     }
 }
--- DreamProperties/DreamProperties/Modules/Settings/SettingsViewModel.cs
+++ DreamProperties/DreamProperties/Modules/Settings/SettingsViewModel.cs
@@ -2,11 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace DreamProperties.Modules.Settings
 {
     public class SettingsViewModel: BaseViewModel
     {
+        public AsyncCommand LogoutCommand { get => new AsyncCommand(PerformLogout); }
 
+        private Task PerformLogout()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
